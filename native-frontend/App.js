30661b79a97b7095321a87ce010a3b5d07b3b22c
Samved <ssjoshi536@gmail.com>
2023-02-01 19:25:23 +0530

    feature: React Native project initialized

---
+++ native-frontend/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+export default function App() {
+   
+  return (
+    <View>
+      <Text>Hello there!</Text>
+    </View>
+  )
+};
